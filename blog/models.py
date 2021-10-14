aa0a7d761f7773b833894587cf92ec579a9c676f
Deeraj Kumar <deerajkmr98@gmail.com>
2021-10-14 22:37:44 +0530

    renamed approved comment to approve comment

--- blog/models.py
+++ blog/models.py
@@ -8,7 +8,7 @@ class Post(models.Model):
     author = models.ForeignKey("auth.User",on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     text = models.TextField()
-    create_date = models.DateTimeField(default=timezone.now())
+    created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True,null=True)
 
     def publish(self):
@@ -16,7 +16,7 @@ class Post(models.Model):
         self.save()
 
     def approve_comments(self):
-        return self.comments.filter(approved_comment=True)
+        return self.comments.filter(approve_comment=True)
 
     def get_absolute_url(self):
         return reverse("post_detail",kwargs={"pk":self.pk})
@@ -29,7 +29,7 @@ class Comment(models.Model):
     post = models.ForeignKey("blog.Post",related_name="comments",on_delete=models.CASCADE)
     author = models.CharField(max_length=50)
     text = models.TextField()
-    create_date = models.DateTimeField(deafult=timezone.now())
+    created_date = models.DateTimeField(default=timezone.now)
     approve_comment = models.BooleanField(default=False)
 
     def approve(self):
